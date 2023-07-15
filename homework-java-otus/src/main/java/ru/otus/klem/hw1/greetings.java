0f22327ac3a65a1ee15599a39b456e28348155ca
Sergey <klem72@mail.ru>
2023-07-15 22:42:01 +0300

    Commit

---
+++ homework-java-otus/src/main/java/ru/otus/klem/hw1/greetings.java
@@ -0,0 +1,11 @@
+package ru.otus.klem.hw1;
+
+public class greetings {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+
+    }
+}
