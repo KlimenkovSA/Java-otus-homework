22eb903202c2d0cab9002a8df3b64c3c64dde4d0
Sergey <klem72@mail.ru>
2023-09-13 14:34:18 +0300

    дз 17

---
+++ homework-java-otus/src/main/java/ru/otus/klem/hw17/Main.java
@@ -0,0 +1,14 @@
+package ru.otus.klem.hw17;
+//Из Предварительно отсортированного списка (List) сформировать двоичное дерево поиска
+//Написать рекурсивную функцию поиска в сформированном дереве
+//Класс должен имплементировать следующий интерфейс
+//public interface SearchTree {
+        ///**
+        // @param element to find
+        // @return element if exists, otherwise - null
+        // /
+        // T find(T element);
+        // List getSortedList();
+// }
+public class Main {
+}
